6dc8d3d0b5f50b14075388717f8e67866e4cb150
Teddy Andrianina <tedan.h0nzra@gmail.com>
2026-02-28 15:14:05 +0300

    finished pythonmodule00

--- ex7/ft_seed_inventory.py
+++ ex7/ft_seed_inventory.py
@@ -1,1 +1,13 @@
-def ft_seed_inventory(seed_type: str, quantity: int, unit:str) -> None: 
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+
+    tails = ""
+    if unit == "packets":
+        tails = f"{quantity} packets available"
+    elif unit == "grams":
+        tails = f"{quantity} grams total"
+    elif unit == "area":
+        tails = f"covers {quantity} square meters"
+    else:
+        print("Unknown unit type")
+        return
+    print(f"{seed_type.capitalize()}: {tails}")
